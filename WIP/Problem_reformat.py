6426bbec04e19bbfd91d87d2fb11c8422e0dd725
Centurion256 <Vkuchynskyy@gmail.com>
2019-03-03 23:21:36 +0200

    Fexed \int Integrals, changed parenthesis, added \displaystyle

--- WIP/Problem_reformat.py
+++ WIP/Problem_reformat.py
@@ -5,14 +5,23 @@ class Problem():
         def tex_improve(tex):
 
             import re
-            tex = re.sub(r"\$ \$|\$", r"\$\$", tex)
-            #tex = re.sub("(?m)\$ \$|(?<=^\s?)\$", "\\[", tex)
-            #tex = re.sub("(?m)\$(?=$)", "\\]", tex)
+            #tex = re.sub(r"\$ \$|\$", r"\$\$", tex)
+            tex = re.sub("(?m)(?<=^)(\$|\$ \$)", "\\[\\displaystyle", tex)
+            tex = re.sub("(?m)\$(?=$)", "\\]", tex)
+            for function in ['sin', 'cos', 'sec', 'cosec', 'tan', 'cot']:
+
+                tex = re.sub('(?<=\b)|(?<!mathrm)\{%s\}' %(function), '\\{}'.format(function), tex)
+
+            if self.type == 'Trigonometric Integration' and '+C' not in tex:
+
+                    tex = re.sub('\\displaystyle', '\\displaystyle \\int{.+}(?=dx)', tex) 
+
             return tex
         
         from MathMl2LaTeX import mml2latex
 
         self.id = js['id']
+        self.type = js['topic']
         try:
             self.question = tex_improve(mml2latex(js['question']))
             self.choices = [tex_improve(mml2latex(choice)) for choice in js['choices']]
